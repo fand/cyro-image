095d6776ec194825c171838ce7149d7235c77b6b
fand <fand@gmork.in>
2015-10-13 02:36:10 +0900

    F

--- src/js/index.js
+++ src/js/index.js
@@ -25,7 +25,7 @@ const loadSample = (url) => {
 };
 
 
-loadSample('/wav/E.wav');
+loadSample('/wav/F.wav');
 
 const W = 2000;
 const H = 1000;
@@ -63,6 +63,8 @@ emitter.on('sampleLoadSucceeded', (buffer) => {
     vv = last*v < 0 ? 10000: 0;
     let vvv = vv > 10 ? 100 : 0;
     c.fillRect(i, (i*v << 2 *W% 139)%W, 1, (vvv * i * 319)%H);
+    c.fillRect((i *171)%W, ((i*vvv << 21 *W)% 42)%H+270, 0.3, (vvv * i * 319)%H);
+    c.fillRect((i *371)%W, ((vvv*i*i*i << 37 *W)% 67)%H+630, 0.3, (vvv * i * 319)%H);
     last = v;
   });
 
